44dd4ce58c4c23ff56dc50ec26761ab224e42fd3
Drotved <drotved@kathart.dk>
2022-02-23 22:15:18 +0100

    Created project

---
+++ MorseCodeTranslator/MorseCodeTranslator/Program.cs
@@ -0,0 +1,64 @@
+﻿using MorseCodeTranslator.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MorseCodeTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letterToMorsedictionary = JsonDictionaryService.GetLetterToMorseDictionaryFromJson();
+
+            Console.WriteLine("Starting Translator ... ");
+
+            Start(letterToMorsedictionary);
+            
+        }
+
+        private static void Start(Dictionary<string, string> dictionary)
+        {
+            while (true)
+            {
+                var input = GetInput("Enter sentence to convert to morse:\n");
+
+                var morseString = GetMorseFromString(dictionary, input);
+
+                var stringFromMorse = GetStringFromMorse(dictionary, morseString);
+
+                var finalInput = GetInput("Try again?");
+                if (finalInput.ToLower() == "no")
+                {
+                    break;
+                }
+            }
+        }
+
+        private static string GetInput(string description)
+        {
+            Console.WriteLine(description);
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        private static string GetMorseFromString(Dictionary<string, string> dictionary, string input)
+        {
+            Console.WriteLine("\nSentence converted to Morse:");
+            var morseString = JsonDictionaryService.GetMorseCodeFromString(dictionary, input);
+            Console.WriteLine(morseString);
+
+            return morseString;
+        }
+
+        private static string GetStringFromMorse(Dictionary<string, string> dictionary, string morseString)
+        {
+            Console.WriteLine("\nMorse converted back to sentence:");
+            var stringFromMorse = JsonDictionaryService.GetStringFromMorseCode(dictionary, morseString);
+            Console.WriteLine($"{stringFromMorse}\n");
+
+            return stringFromMorse;
+        }
+    }
+}
---
+++ MorseCodeTranslator/MorseCodeTranslator/Services/JsonDictionaryService.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MorseCodeTranslator.Services
+{
+    public static class JsonDictionaryService
+    {
+        public static Dictionary<string, string> GetLetterToMorseDictionaryFromJson()
+        {
+            using (StreamReader reader = new StreamReader("../../json/alphabetToMorse.json"))
+            {
+                string json = reader.ReadToEnd();
+                var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+                return dictionary;
+            }
+        }
+
+        public static void PrintDictionary(Dictionary<string, string> dictionary)
+        {
+            foreach (var character in dictionary)
+            {
+                Console.WriteLine($"Key is: {character.Key}\nValue is: {character.Value}\n");
+            }
+        }
+
+        public static string GetValueFromDictionary(Dictionary<string, string> dictionary, string key)
+        {
+            dictionary.TryGetValue(key, out string value);
+            return value;
+        }
+
+        public static string GetValueFromDictionary(Dictionary<string, string> dictionary, char key)
+        {
+            dictionary.TryGetValue(key.ToString(), out string value);
+            return value;
+        }
+
+        public static string GetKeyFromDictionary(Dictionary<string, string> dictionary, string value)
+        {
+            var key = dictionary.FirstOrDefault(KVpair => KVpair.Value == value).Key;
+            return key;
+        }
+
+        public static string GetMorseCodeFromString(Dictionary<string, string> dictionary, string sentence)
+        {
+            var morseSentence = string.Empty;
+            char[] characters = sentence.ToCharArray();
+
+            foreach (var character in characters)
+            {
+                morseSentence = $"{morseSentence}|{GetValueFromDictionary(dictionary, character)}";
+            }
+
+            return morseSentence;
+        }
+
+        public static string GetStringFromMorseCode(Dictionary<string, string> dictionary, string morseString)
+        {
+            var sentence = string.Empty;
+            string[] morseLetters = morseString.Split('|');
+
+            foreach (var character in morseLetters)
+            {
+                sentence = $"{sentence}{GetKeyFromDictionary(dictionary, character)}";
+            }
+
+            return sentence;
+        }
+
+    }
+}
